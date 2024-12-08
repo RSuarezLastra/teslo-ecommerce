de6b2cb8cd96fd1687bf1516658bae3ae348ebcf
RSLastra <raul.slastra@gmail.com>
2024-12-08 13:44:27 -0600

    feat: config fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
     <div className="">
       <h2>Hola mundo</h2>
+      <h2 className={`${titleFont.className} font-bold`}>Hola mundo</h2>
     </div>
   );
 }
