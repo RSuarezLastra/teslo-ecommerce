97f06e8aef9ac17898f6cf808cae29c919cbc9be
RSLastra <raul.slastra@gmail.com>
2024-12-10 20:23:00 -0600

    feat: ProductsGrid components

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,14 +1,21 @@
-import { Title } from "@/components";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
 
+const products = initialData.products;
 
 export default function Home() {
   return (
-    <div className="">
+    <>
       <Title
         title="Tienda"
         subtitle="Todos los productos"
         className="mb-2"
-        />
-    </div>
+      />
+
+      <ProductGrid
+        products={products}
+      />
+      
+    </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,9 @@
-export * from './ui/top-menu/TopMenu'; 
-export * from './ui/not-found/PageNotFound'; 
+//? ui
+
+export * from './ui/top-menu/TopMenu';
+export * from './ui/not-found/PageNotFound';
 export * from './ui/title/Title';
+
+//? products
+
+export * from './products/product-grid/ProductGrid';
