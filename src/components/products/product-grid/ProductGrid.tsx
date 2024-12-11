3aec9835c7618e704d1441384050111ed8f07c24
RSLastra <raul.slastra@gmail.com>
2024-12-10 21:03:58 -0600

    ProductGridItem

--- src/components/products/product-grid/ProductGrid.tsx
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/interfaces"
+import { ProductGridItem } from "./ProductGridItem";
 
 interface Props {
   products: Product[];
@@ -8,10 +9,13 @@ interface Props {
 export const ProductGrid = ({ products }: Props) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
-      
+
       {
-        products.map( product => (
-          <span key={product.slug}>{product.title}</span>
+        products.map(product => (
+          <ProductGridItem
+            key={product.slug}
+            product={product}
+          />
         ))
       }
 
