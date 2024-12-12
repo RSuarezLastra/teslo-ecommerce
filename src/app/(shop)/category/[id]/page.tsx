99e6c8985f81230cf37316f2c99e4df66ec0cc4c
RSLastra <raul.slastra@gmail.com>
2024-12-11 19:58:00 -0600

    category page, change some interfaces

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,22 +1,42 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   }
 }
+
 
 export default async function ({ params }: Props) {
 
   const { id } = await params;
 
-  if(id === "kids"){
-    notFound();
+  const products = initialData.products.filter(product => product.gender === id);
+
+  const label: Record<Category, string> = {
+    "men": "Hombres",
+    "women": "Mujeres",
+    "kid": "Niños",
+    "unisex": "todos"
   }
+
+  // if(id === "kids"){
+  //   notFound();
+  // }
 
   return (
     <div>
-      <h2>Category Page {id}</h2>
+      <Title
+        title={`Articulos para ${label[id]}`}
+        className="mb-2"
+      />
+
+      <ProductGrid
+        products={products}
+      />
     </div>
   );
 }
