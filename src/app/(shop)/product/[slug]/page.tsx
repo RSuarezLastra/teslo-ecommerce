b86dff668c845ab98f13a84974895de3273d97ed
RSLastra <raul.slastra@gmail.com>
2024-12-12 10:22:16 -0600

    feat: starting product page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,46 @@
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
 
-export default function() {
+interface Props {
+  params: {
+    slug: string;
+  }
+}
+
+export default async function ({ params }: Props) {
+
+  const { slug } = await params;
+
+  const product = initialData.products.find(product => product.slug === slug);
+
   return (
-    <div>
-      <h1>Producto - 324</h1>
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+
+      {/* SlidesShow */}
+      <div className="col-span-1 md:col-span-2">
+        <h1>Hola mundo</h1>
+      </div>
+
+      {/* Detalles */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product?.title}
+        </h1>
+        <p className="text-lg mb-5">${product?.price}</p>
+
+        {/* Selector de Tallas */}
+
+        <button className="btn-primary my-5">
+          Agregar al carrito
+        </button>
+
+        {/* Descripcion */}
+        <h3 className="font-bold text-sm">Descripción</h3>
+        <p className="font-light">
+          {product?.description}
+        </p>
+
+      </div>
     </div>
   );
 }
