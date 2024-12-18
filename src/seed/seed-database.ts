38c5311a188edbc6ac31be0d77565c605e52bcad
RSLastra <raul.slastra@gmail.com>
2024-12-17 23:25:09 -0600

    feat: seed database finish

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -4,19 +4,51 @@ import prisma from "../lib/prisma";
 
 async function main() {
 
+  const { categories, products } = initialData;
+
+  // delete data
   await Promise.all([
     prisma.productImage.deleteMany(),
     prisma.product.deleteMany(),
     prisma.category.deleteMany(),
   ]);
 
-  const { categories } = initialData
-
+  // categories Seed
   const categoriesData = categories.map(name => ({ name }));
 
   await prisma.category.createMany({
     data: categoriesData
-  })
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  //products seed
+  products.forEach(async (product) => {
+
+    const { type, images, ...rest } = product;
+
+    const productDb = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type]
+      }
+    });
+
+    const imagesData = images.map(image => ({
+      url: image,
+      productId: productDb.id
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData
+    })
+
+  });
 
   console.log('Seed ejecutado correctamente');
 }
