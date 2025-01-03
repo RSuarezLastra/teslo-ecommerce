7955c39d1666bbc81169098383ab0f6c4b2a5454
RSLastra <raul.slastra@gmail.com>
2025-01-03 14:18:19 -0600

    feat: connect loginform with actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,5 @@
 export * from './products/products-pagination';
 export * from './products/get-product-by-slug';
 export * from './products/get-stock-by-slug';
+
+export * from './auth/login';
--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function () {
   return (
-    <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
+    <div className="flex flex-col min-h-screen pt-24">
 
       <h1 className={ `${ titleFont.className } text-4xl mb-5` }>Nueva cuenta</h1>
 
