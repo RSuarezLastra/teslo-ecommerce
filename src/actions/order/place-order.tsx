9573a60ac21476978c181bb1feb7736390dbc36e
RSLastra <raul.slastra@gmail.com>
2025-01-10 22:04:10 -0600

    feat: calculate totals for order

--- src/actions/order/place-order.tsx
+++ src/actions/order/place-order.tsx
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth.config";
 import { Address, Size } from "@/interfaces";
+import prisma from "@/lib/prisma";
 
 interface ProductToOrder {
   productId: string;
@@ -21,6 +22,32 @@ export const placerOrder = async (productsId: ProductToOrder[], address: Address
     }
   }
 
-  console.log(productsId, address, userId);
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productsId.map(p => p.productId)
+      }
+    }
+  });
 
+  const itemsInOrder = productsId.reduce((count, product) => count + product.quantity, 0)
+
+  const { subTotal, tax, total } = productsId.reduce((totals, item) => {
+
+    const productQuantity = item.quantity;
+    const product = products.find(product => product.id === item.productId);
+
+    if (!product) throw new Error(`El producto con el id: ${item.productId} no existe - 500`)
+
+    const subTotal = product.price * productQuantity;
+
+    totals.subTotal += subTotal;
+    totals.tax += (subTotal * 0.16);
+    totals.total += (subTotal * 1.16);
+
+    return totals;
+  }, { subTotal: 0, tax: 0, total: 0 })
+
+  console.log(subTotal, tax, total);
+  
 }
