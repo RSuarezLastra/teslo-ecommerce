4536f8292859f5d9b55afdb0e798d3e00545a0cc
RSLastra <raul.slastra@gmail.com>
2025-01-16 13:37:25 -0600

    feat: orders page

---
+++ src/actions/order/get-orders-by-user.ts
@@ -0,0 +1,44 @@
+import { auth } from "@/auth.config"
+import prisma from "@/lib/prisma";
+
+
+export const getOrdersByUser = async () => {
+  const session = await auth();
+
+  try {
+
+    if (session?.user) {
+      return {
+        ok: false,
+        message: 'Debe estar autenticado'
+      }
+    }
+
+    const orders = await prisma.order.findMany({
+      where: {
+        UserId: session?.user.id
+      },
+      include: {
+        OrderAddress: {
+          select: {
+            firstName: true,
+            lastName: true
+          }
+        }
+      }
+    });
+
+    return {
+      ok: true,
+      orders: orders
+    }
+
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: 'Error al cargar ordenes'
+    }
+
+  }
+}
