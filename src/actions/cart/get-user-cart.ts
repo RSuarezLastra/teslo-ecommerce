0e6ce79552a206b6a2028f2c3094786930f3a22d
RSLastra <raul.slastra@gmail.com>
2025-02-07 20:06:59 -0600

    getUserCart action

--- src/actions/cart/get-user-cart.ts
+++ src/actions/cart/get-user-cart.ts
@@ -1,9 +1,19 @@
 'use server'
 
+import prisma from "@/lib/prisma";
 import { User } from "@/interfaces"
 
 export const getUserCart = async (userId: User['id']) => {
   try {
+
+    const cart = await prisma.cart.findUnique({
+      where: { userId: userId },
+      include: {
+        CartProduct: true
+      }
+    });
+
+    return cart;
 
   } catch (error) {
     console.log(error);
